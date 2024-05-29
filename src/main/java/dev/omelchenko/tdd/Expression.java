04a75cefa1c29c6e2fc47ab275a3431cd2b670e8
Pavel Omelchenko <p.Omelchenko@gmail.com>
2024-05-29 21:51:24 +0300

    copy tests and code from internet

---
+++ src/main/java/dev/omelchenko/tdd/Expression.java
@@ -0,0 +1,7 @@
+package dev.omelchenko.tdd;
+
+public interface Expression {
+    public Money reduce(Bank bank, String to);
+    public Expression plus(Expression addend);
+    public Expression times(int multiplier);
+}
